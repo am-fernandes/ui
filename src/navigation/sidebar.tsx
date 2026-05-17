3c68a53eec8920190f56219f983cd02adf88e19d
matthsena <matheusalexandre.sena99@gmail.com>
2026-05-17 20:41:59 -0300

    feat(navigation)!: remove sm/lg size variants from SidebarMenuButton

--- src/navigation/sidebar.tsx
+++ src/navigation/sidebar.tsx
@@ -457,8 +457,6 @@ const sidebarMenuButtonVariants = cva(
       },
       size: {
         default: "h-8 text-sm",
-        sm: "h-7 text-xs",
-        lg: "h-12 text-sm group-data-[collapsible=icon]:p-0!",
       },
     },
     defaultVariants: {
