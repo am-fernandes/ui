e99cadbddaa0d3c123a4581738867ce9f7817428
giovanniclopes <giovanniclopes@gmail.com>
2026-05-21 14:06:03 -0300

    fix(toggle): enhance toggle interaction with active scaling effect
    Updated the toggle component styles to include an active scaling effect, improving user interaction. The toggle now scales down slightly when active, ensuring a more responsive experience while maintaining consistent behavior when disabled.

--- src/primitives/toggle.tsx
+++ src/primitives/toggle.tsx
@@ -25,7 +25,7 @@ import { cn } from "@/lib/utils"
  * standard.
  */
 const toggleVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium cursor-pointer transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium cursor-pointer transition focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 active:scale-[0.98] disabled:pointer-events-none disabled:opacity-50 disabled:active:scale-100 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border",
   {
     variants: {
       variant: {
